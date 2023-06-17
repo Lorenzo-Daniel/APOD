6d7d20e8bcd6f14158e14e98d1add0b42aa13920
unknown <cogitaciones1@gmail.com>
2023-06-17 02:05:34 -0300

    fixed

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle, styled } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap");
