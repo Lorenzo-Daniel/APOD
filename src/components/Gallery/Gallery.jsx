b63bc6dff02604484476a610d4fbfd3063e675ef
unknown <cogitaciones1@gmail.com>
2023-06-20 14:22:46 -0300

    any search style

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -29,7 +29,7 @@ function Gallery() {
       showConfirmButton: false,
       showCloseButton: true,
       position: "center",
-      background: '#ffffff',
+      background: "#ffffff",
       showClass: {
         popup: "animate__animated animate__fadeIn animate__fast",
       },
@@ -125,18 +125,20 @@ function Gallery() {
       )}
 
       {showResults ? (
-        <div style={{ backgroundColor: "#67e8fe" }}>
+        <div style={{ backgroundColor: "#67e8fe",height:'100vh' }}>
           <div className="ms-2  d-flex flex-column align-items-start">
             <Link to={"/"} className="btn btn-small p-1 pb-0">
               <i className="fa-regular fa-hand-point-right fs-2 mb-0 text-light" />
             </Link>
             <Span className="text-white">Panel</Span>
           </div>
-          <div>
-            <p className="text-white fs-md-2 fs-4 pt-5 m-0">
+          <div className="d-flex flex-column p-3">
+            <p className="text-white fs-md-2 fs-4 pt-5 m-0 text-center">
               You have not selected any search option!
             </p>
-            <img src={astronaut} alt="astronaut" className="img-fluid w-50 " />
+            <div className="d-flex justify-content-center ">
+            <img src={astronaut} style={{ maxWidth:'350px'}} alt="astronaut" className="img-fluid w-50" />
+            </div>
           </div>
         </div>
       ) : (
@@ -172,7 +174,7 @@ function Gallery() {
                       id={index}
                       src={object.url}
                       className="img-fluid w-100 p-3"
-                      style={{cursor:'pointer'}}
+                      style={{ cursor: "pointer" }}
                       alt={object.title}
                       onClick={() => popUpImgGaleria(object.url, object.title)}
                     />
@@ -181,7 +183,7 @@ function Gallery() {
                 </div>
                 <div className="col-12 col-lg-9 m-auto mb-4">
                   <h6 className="p-3 montserrat text-center">{object.title}</h6>
-                  <div className="border rounded-2 mx-3 col-md-11  m-auto">
+                  <div className="border rounded-2 mx-3  m-auto">
                     <button
                       className="btn w-100 d-flex justify-content-between align-items-center"
                       onClick={() => dropDownExplanationHandler(index)}
