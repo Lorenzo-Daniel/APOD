b568f88a98e18a985a8fb96b43d751c1e2dcdcbc
unknown <cogitaciones1@gmail.com>
2023-06-20 13:16:56 -0300

    swal2

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import astronaut from "../Gallery/astronaut.jpg";
 import { Span } from "./styles";
 import ReactPlayer from "react-player";
+import Swal from "sweetalert2";
 // import { Div, Button } from "../../styles/GlobalStyles";
 
 function Gallery() {
@@ -13,11 +14,35 @@ function Gallery() {
   const [spinner, setSpinner] = useState(true);
   const [showResults, setShowResults] = useState(null);
   const pageRef = useRef(null);
-  
+
   const scrollToTop = () => {
     if (pageRef.current) {
-      pageRef.current.scrollIntoView({ behavior: 'smooth' });
+      pageRef.current.scrollIntoView({ behavior: "smooth" });
     }
+  };
+
+  const popUpImgGaleria = (urlImg, nombre) => {
+    Swal.fire({
+      title: `${nombre}`,
+      color: "#ffffff",
+      imageUrl: urlImg,
+      showConfirmButton: false,
+      showCloseButton: true,
+      position: "center",
+      background: '#ffffff',
+      showClass: {
+        popup: "animate__animated animate__fadeIn animate__fast",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOut animate__fast",
+      },
+      customClass: {
+        popup: "galeria-swal2-popup",
+        closeButton: "swal2-close ",
+        title: "galeria-swal2-title",
+        image: "galeria-swal2-image",
+      },
+    });
   };
 
   //WIKIPEDIA
@@ -55,8 +80,6 @@ function Gallery() {
     setDropDownExplanationIndex(index);
   };
 
- 
-
   useEffect(() => {
     const getLS = JSON.parse(localStorage.getItem("pictures")) || [];
     //OBTENER TITLES APOD
@@ -88,7 +111,6 @@ function Gallery() {
         console.error(error);
       }
     };
-
     getTitlesApod(getLS);
   }, []);
 
@@ -126,7 +148,11 @@ function Gallery() {
             <Span className="text-black">Panel</Span>
           </div> */}
           <div className=" p-3  d-flex flex-column align-items-end sticky-top">
-            <Link to={"/"} className="btn btn-small p-1 pb-0  bg-info" onClick={scrollToTop}>
+            <Link
+              to={"/"}
+              className="btn btn-small p-1 pb-0  bg-info"
+              onClick={scrollToTop}
+            >
               <i className="fa-regular fa-hand-point-up fs-2 mb-0 text-black" />
             </Link>
           </div>
@@ -135,18 +161,20 @@ function Gallery() {
               <div key={index}>
                 <div className="p-lg-2 d-flex flex-column col-lg-9 align-items-center m-auto">
                   {object.media_type === "video" ? (
-                  <ReactPlayer
-                    url = {object.url}
-                    controls
-                    width={'100%'}
-                    style={{padding:'18px'}}
-                  />
+                    <ReactPlayer
+                      url={object.url}
+                      controls
+                      width={"100%"}
+                      style={{ padding: "18px" }}
+                    />
                   ) : (
                     <img
                       id={index}
                       src={object.url}
-                      className="img-fluid w-100 p-3  "
+                      className="img-fluid w-100 p-3"
+                      style={{cursor:'pointer'}}
                       alt={object.title}
+                      onClick={() => popUpImgGaleria(object.url, object.title)}
                     />
                   )}
                   <p className="mt-1 mb-0">{object.date}</p>
@@ -175,10 +203,7 @@ function Gallery() {
                     {wiki[index].map((element, i) => {
                       return (
                         <div
-                          style={{
-                            overflow: "hidden",
-                          }}
-                          className=" border rounded my-2 mx-3 p-2"
+                          className=" border rounded my-2 mx-3 p-2 text-center"
                           key={i}
                         >
                           <a
